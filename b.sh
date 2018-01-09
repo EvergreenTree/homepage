920dbb8d4c532ea19255e4305390a285c876f377
EvergreenTree <627430293@qq.com>
2018-01-09 22:27:10 +0100

    seam carving

--- b.sh
+++ b.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 cd /Users/EvergreenFu/GitHub/homepage/
 
+python2 jemdoc.py index.jemdoc;
 python2 jemdoc.py gradthesis.jemdoc;
 python2 jemdoc.py NDE.jemdoc;
 python2 jemdoc.py stat.jemdoc;
