6ba54bff84e0b5be9ab8ceb281c9f74220ed4417
Evergreen <evergreen@Evergreens-MacBook-Air.local>
2025-09-17 15:58:15 +0200

    drop recompliation of tfjs-vis

--- src/types/glamor.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'glamor' {
-  // Minimal typing to satisfy usage in this project
-  // css(...) returns a className string
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function css(...rules: any[]): string;
-  // glamor v2 exposes `style`, which we alias to css in code
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export function style(...rules: any[]): string;
-}
